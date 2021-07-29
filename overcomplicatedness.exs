7b62ca908d8653738cf8ddac1bb6c41a32904c8a
wij <41879253+wijiler@users.noreply.github.com>
2021-07-29 18:18:56 -0400

    hmm not overcomplicated enough

---
+++ overcomplicatedness.exs
@@ -0,0 +1,10 @@
+# we could just do intname = 8 but thats for dumb stupid scrubs
+
+defmodule interger_stuff do
+  def intergercomplicated do
+    intergerhere = String.length("funni bd")
+      IO.puts(intergerhere)
+    end
+end
+
+interger_stuff
